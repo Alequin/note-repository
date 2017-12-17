0a918246136361add215c4d52bd149ab4ab08897
James Cox <j-cox03@hotmail.com>
2017-12-17 12:23:23 +0000

    Made fake data to work with

---
+++ src/dev/fakeSummaryNotesData.js
@@ -0,0 +1,22 @@
+export default (
+  [
+    {
+      title: "title1",
+      summary: "this is a summary",
+      tags: ["tag1", "tag2", "tag3", "tag4"],
+      creationDate: "2017-05-01"
+    },
+    {
+      title: "title2",
+      summary: "this is also a summary",
+      tags: ["tag1", "tag2", "tag3", "tag4"],
+      creationDate: "2017-08-25"
+    },
+    {
+      title: "title3",
+      summary: "all the summary",
+      tags: ["tag1", "tag2", "tag3", "tag4"],
+      creationDate: "2016-01-12"
+    },
+  ]
+)
