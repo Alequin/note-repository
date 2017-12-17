b33992f13a96bf7012271766eedb0ec3297f56a7
James Cox <j-cox03@hotmail.com>
2017-12-17 11:41:25 +0000

    MainPage: used nav bar

--- src/containers/mainPage/MainPage.jsx
+++ src/containers/mainPage/MainPage.jsx
@@ -1,9 +1,11 @@
 import React from "react"
+import Nav from "./../nav/Nav.jsx"
 
 class MainPage extends React.Component{
   render(){
     return (
-      <div className="main-page">
+      <div className="main-page-frame">
+          <Nav />
       </div>
     )
   }
