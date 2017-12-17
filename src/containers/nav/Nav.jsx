40644cebdac2f0905334c758eec5d6380c18c473
James Cox <j-cox03@hotmail.com>
2017-12-17 11:40:09 +0000

    Made shell of nav container

---
+++ src/containers/nav/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+class Nav extends React.Component{
+  render(){
+    return (
+      <div className="nav-bar-frame">
+        nav bar
+      </div>
+    )
+  }
+}
+
+export default Nav
