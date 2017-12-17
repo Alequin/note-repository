4c40fadc07556fa075d4820ad1294992cad25391
James Cox <j-cox03@hotmail.com>
2017-12-17 11:49:49 +0000

    Included nav styling from template

--- src/containers/nav/Nav.jsx
+++ src/containers/nav/Nav.jsx
@@ -4,7 +4,33 @@ class Nav extends React.Component{
   render(){
     return (
       <div className="nav-bar-frame">
-        nav bar
+
+        <div className="nav-chain-frame">
+          <img src={require("./../../../build/images/chain-crop-2.png")}/>
+          <span className="nav-chain-spacing"></span>
+          <img src={require("./../../../build/images/chain-crop-2.png")}/>
+        </div>
+
+        <nav>
+          <div className="nav-buttons-frame">
+            <div>
+              <a href="#">Home</a>
+              <a href="#">Select Filter Tags</a>
+            </div>
+            <div>
+              <a href="#">Clear Filter Tags</a>
+              <a href="#">View Random Note</a>
+            </div>
+          </div>
+
+          <div className="search-frame">
+            <label>
+              <input type="search"></input>
+              <i className="search-icon fa fa-search"></i>
+            </label>
+          </div>
+        </nav>
+
       </div>
     )
   }
