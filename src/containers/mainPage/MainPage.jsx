5abc714c35ee0241683af7afc0acec58ef9c51a6
James Cox <j-cox03@hotmail.com>
2017-12-18 08:07:07 +0000

    Added second set of mock summaries

--- src/containers/mainPage/MainPage.jsx
+++ src/containers/mainPage/MainPage.jsx
@@ -3,14 +3,14 @@ import Nav from "./../nav/Nav.jsx"
 import SelectionPage from "./../selectionPage/SelectionPage.jsx"
 
 //DEV TO REMOVE
-import {mockNoteSummeries1} from "./../../dev/fakeSummaryNotesData.js"
+import {mockNoteSummeries2} from "./../../dev/fakeSummaryNotesData.js"
 
 class MainPage extends React.Component{
 
   constructor(props){
     super(props)
     this.state = {
-      noteSummaries: mockNoteSummeries1
+      noteSummaries: mockNoteSummeries2
     }
   }
 
--- src/dev/fakeSummaryNotesData.js
+++ src/dev/fakeSummaryNotesData.js
@@ -20,3 +20,38 @@ export const mockNoteSummeries1 = (
     },
   ]
 )
+
+export const mockNoteSummeries2 = (
+  [
+    {
+      title: "title1",
+      summary: "this is a summary this is a summary this is a summary this is a summary this is a summary this is a summary ",
+      tags: ["tag1", "tag2", "tag3", "tag4"],
+      creationDate: "2017-05-01"
+    },
+    {
+      title: "title2",
+      summary: "this is also a summary",
+      tags: ["tag1", "tag2", "tag3", "tag4"],
+      creationDate: "2017-08-25"
+    },
+    {
+      title: "title3",
+      summary: "all the summary",
+      tags: ["tag1", "tag2", "tag3", "tag4"],
+      creationDate: "2016-01-12"
+    },
+    {
+      title: "title4",
+      summary: "all the summary",
+      tags: ["tag1", "tag2", "tag3", "tag4"],
+      creationDate: "2016-01-12"
+    },
+    {
+      title: "title5",
+      summary: "all the summary",
+      tags: ["tag1", "tag2", "tag3", "tag4"],
+      creationDate: "2016-01-12"
+    },
+  ]
+)
