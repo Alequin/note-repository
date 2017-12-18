24a584b7e7574bb546f121a41663bc85d24654e6
James Cox <j-cox03@hotmail.com>
2017-12-18 06:59:42 +0000

    Changed how dev mock notes are used

--- src/dev/fakeSummaryNotesData.js
+++ src/dev/fakeSummaryNotesData.js
@@ -1,4 +1,4 @@
-export default (
+export const mockNoteSummeries1 = (
   [
     {
       title: "title1",
--- src/specs/containers/SelectionPageSpecs.test.js
+++ src/specs/containers/SelectionPageSpecs.test.js
@@ -1,6 +1,6 @@
 import assert from "assert"
-import React from 'react';
-import mockNotes from "./../../dev/fakeSummaryNotesData.js"
+import React from 'react'
+import {mockNoteSummeries1} from "./../../dev/fakeSummaryNotesData.js"
 import SelectionPage from "./../../containers/selectionPage/SelectionPage.jsx"
 import NoteSummary from "./../../components/noteSummary/NoteSummary.jsx"
 
@@ -9,24 +9,24 @@ describe("Selection Page", function(){
   let page
 
   beforeEach(() => {
-    page = new SelectionPage(mockNotes)
+    page = new SelectionPage(mockNoteSummeries1)
   })
 
   it("can render single summary note", () => {
-    let expected = <NoteSummary key="0" note={mockNotes[0]}/>
-    let result = page.buildSingleSummary(0, mockNotes[0])
+    let expected = <NoteSummary key="0" note={mockNoteSummeries1[0]}/>
+    let result = page.buildSingleSummary(0, mockNoteSummeries1[0])
     assert.deepEqual(result, expected)
   })
 
   it("can render list of summary notes", () => {
-    const page = new SelectionPage(mockNotes)
+    const page = new SelectionPage(mockNoteSummeries1)
 
     let expected = [
-      <NoteSummary key="0" note={mockNotes[0]}/>,
-      <NoteSummary key="1" note={mockNotes[1]}/>,
-      <NoteSummary key="2" note={mockNotes[2]}/>
+      <NoteSummary key="0" note={mockNoteSummeries1[0]}/>,
+      <NoteSummary key="1" note={mockNoteSummeries1[1]}/>,
+      <NoteSummary key="2" note={mockNoteSummeries1[2]}/>
     ]
-    let result = page.buildSummaries(mockNotes)
+    let result = page.buildSummaries(mockNoteSummeries1)
     assert.deepEqual(result, expected)
   })
 })
