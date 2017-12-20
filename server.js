9d61ab1bec0cce2e37b62ee73e6c53790a74f3ac
James Cox <j-cox03@hotmail.com>
2017-12-20 20:34:36 +0000

    Started using pg

--- server.js
+++ server.js
@@ -17,4 +17,4 @@ app.use(require("./controllers/index.js"))
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', PORT)
-});
+})
---
+++ services/PostgresConnector.js
@@ -0,0 +1,26 @@
+const pg = require('pg')
+
+function PostgresConnector(path){
+  this.connectionString = path
+}
+
+PostgresConnector.prototype.connect = function (sql) {
+  // https://node-postgres.com/guides/upgrading
+  var pool = new pg.Pool({
+      connectionString: this.connectionString
+  })
+
+  const promise = new Promise((resolve, reject) => {
+    pool.connect().then((client) => {
+      client.query(sql.command, sql.values, (err, res) => {
+        if (err) reject(err)
+        else resolve(res)
+        client.end()
+      })
+    })
+  })
+  
+  return promise
+}
+
+module.exports = PostgresConnector
