5f4f11e929c3dd795def7ef9c1e67fd4ddf46cf4
James Cox <j-cox03@hotmail.com>
2017-12-20 17:18:16 +0000

    Added server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+const PORT = 5000
+
+app.set('port', PORT);
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', PORT);
+});
