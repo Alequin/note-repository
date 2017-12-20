3f7cc6f78bbc12f1b9bed8c0f299fe6c8154b398
James Cox <j-cox03@hotmail.com>
2017-12-20 12:59:07 +0000

    Made tage css more globally impactful

--- src/ImportAllScss.js
+++ src/ImportAllScss.js
@@ -1,5 +1,8 @@
 import appCss from "./App.scss"
+
 import navCss from "./containers/nav/Nav.scss"
 import selectionPageCss from "./containers/selectionPage/SelectionPage.scss"
-import noteSummaryCss from "./components/noteSummary/NoteSummary.scss"
 import viewPageCss from "./containers/viewPage/ViewPage.scss"
+
+import noteSummaryCss from "./components/noteSummary/NoteSummary.scss"
+import tagsCss from "./components/tags/Tags.scss"
