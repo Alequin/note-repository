c70119d71339344b85f914b288996f0ec599b71e
James Cox <j-cox03@hotmail.com>
2017-12-20 18:54:12 +0000

    Made index controller

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -11,6 +11,10 @@ const PORT = 5000
 
 app.set('port', PORT);
 
+app.use(express.static('./build'));
+
+app.use(require("./controllers/index.js"))
+
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', PORT);
+  console.log('Node app is running on port', PORT)
 });
