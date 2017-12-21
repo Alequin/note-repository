6e863b79d0e572c9a142ace49220bdc220650eda
James Cox <j-cox03@hotmail.com>
2017-12-21 15:38:07 +0000

    Made tags table seeds and corrected use of promises in file seedDatabase

--- scripts/seedDatabase/seedDatabase.js
+++ scripts/seedDatabase/seedDatabase.js
@@ -1,31 +1,50 @@
-import notesTableSeeds from "./notesTableSeeds.js"
 import PostgresConnector from "./../../services/PostgresConnector.js"
 import {databaseInfo} from "./../../settings.js"
 
+import notesTableSeeds from "./notesTableSeeds.js"
+import tagsTableSeeds from "./tagsTableSeeds.js"
+
 import {
-  notesSchema
+  notesSchema,
+  tagsSchema,
 } from "./../../database/schema.js"
 
 const db = new PostgresConnector(databaseInfo.path)
 
 function seedNotesTable(){
   const notesColumns = notesSchema.columns
+  const promises = []
   for(let note of notesTableSeeds){
     const command = `INSERT INTO ${notesSchema.name} `+
       `(${notesColumns[1].name},
         ${notesColumns[2].name},
         ${notesColumns[3].name},
         ${notesColumns[4].name}) `+
-        "VALUES "+
-        `($1, $2, $3, $4);`
+      "VALUES "+
+      `($1, $2, $3, $4);`
     const values = [
       note.title, note.summary, note.file, note.creationDate
     ]
-    return db.connect(command, values)
+    promises.push(db.connect(command, values))
   }
+  return Promise.all(promises)
+}
+
+function seedTagsTable(){
+  const tagsColumns = tagsSchema.columns
+  const promises = []
+  for(let tag of tagsTableSeeds){
+    const command = `INSERT INTO ${tagsSchema.name} `+
+      `(${tagsColumns[1].name})`+
+      "VALUES "+
+      `($1);`
+    const values = [tag.name]
+    promises.push(db.connect(command, values))
+  }
+  return Promise.all(promises)
 }
 
 seedNotesTable()
   .then(() => {
-    
+    return seedTagsTable()
   })
---
+++ scripts/seedDatabase/tagsTableSeeds.js
@@ -0,0 +1,7 @@
+const tagsSeeds = []
+
+for(let j=0; j<20; j++){
+  tagsSeeds.push({name: "tag " + j})
+}
+
+export default tagsSeeds
