c5f03778233fac018cdd6d11a274f1c543b91715
James Cox <j-cox03@hotmail.com>
2017-12-21 10:01:08 +0000

    updated schema for es6

--- database/schema.js
+++ database/schema.js
@@ -1,31 +1,27 @@
-module.exports = {
-
-  notesTable: {
-    name: "notes",
-    columns: {
-      id: {name: "id", type: "SERIAL8"},
-      title: {name: "title",  type: "VARCHAR(127)"},
-      summary: {name: "summary",  type: "TEXT"},
-      file: {name: "file",  type: "VARCHAR(127)"},
-      creation_date: {name: "creation_date",  type: "DATE"}
-    }
-  },
-
-  tagsTable: {
-    name: "tags",
-    columns: {
-      id: {name: "id", type: "SERIAL8"},
-      title: {name: "title",  type: "VARCHAR(127)"}
-    }
-  },
-
-  noteTagsTable: {
-    name: "noteTags",
-    columns: {
-      id: {name: "id", type: "SERIAL8"},
-      note_id: {name: "note_id", type: "SERIAL8"},
-      tag_id: {name: "tag_id", type: "SERIAL8"}
-    }
+export const notesSchema = {
+  name: "notes",
+  columns: {
+    id: {name: "id", type: "SERIAL8"},
+    title: {name: "title",  type: "VARCHAR(127)"},
+    summary: {name: "summary",  type: "TEXT"},
+    file: {name: "file",  type: "VARCHAR(127)"},
+    creation_date: {name: "creation_date",  type: "DATE"}
   }
-  
+}
+
+export const tagsSchema = {
+  name: "tags",
+  columns: {
+    id: {name: "id", type: "SERIAL8"},
+    title: {name: "title",  type: "VARCHAR(127)"}
+  }
+}
+
+export const noteTagsSchema ={
+  name: "noteTags",
+  columns: {
+    id: {name: "id", type: "SERIAL8"},
+    note_id: {name: "note_id", type: "SERIAL8"},
+    tag_id: {name: "tag_id", type: "SERIAL8"}
+  }
 }
