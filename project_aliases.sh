26bf12657342ef7a301ef1b9fcb5fb0e36cf0ea2
James Cox <j-cox03@hotmail.com>
2017-12-21 10:14:34 +0000

    updated terminal commands

---
+++ project_aliases.sh
@@ -0,0 +1,11 @@
+alias strt="npm start"
+alias tst="npm test"
+alias bwp="npm run build-webpack"
+alias bjs="npm run build-es6"
+alias bdb="npm run build-database"
+alias bld=" npm run build-es6; npm run build-webpack"
+alias to-git="open https://github.com/Alequin/note-repository"
+
+alias clear-project-aliases="unalias strt tst bwp bjs bld to-git"
+
+echo "Project aliases loaded"
