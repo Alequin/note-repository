6fd14e51bda2876cd9b25894f53174d05c058382
James Cox <j-cox03@hotmail.com>
2017-12-21 11:06:09 +0000

    Made code to create note tags table

---
+++ scripts/createNoteTagsTable.js
@@ -0,0 +1,21 @@
+import {
+  notesSchema,
+  tagsSchema,
+  noteTagsSchema
+} from "./../database/schema.js"
+
+function createNoteTagsTable(db){
+  const noteTagsColumns = noteTagsSchema.columns
+  const createTable = (
+    `CREATE TABLE ${noteTagsSchema.name} (
+      ${noteTagsColumns[0].name} ${noteTagsColumns[0].type} PRIMARY KEY,
+      ${noteTagsColumns[1].name} ${noteTagsColumns[1].type}
+      REFERENCES ${notesSchema.name}(${notesSchema.columns[0].name}),
+      ${noteTagsColumns[2].name} ${noteTagsColumns[2].type}
+      REFERENCES ${tagsSchema.name}(${tagsSchema.columns[0].name})
+    );`
+  )
+  return db.connect(createTable)
+}
+
+export default createNoteTagsTable
