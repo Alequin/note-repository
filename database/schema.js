d0133458e6fe33a71183a9f4c1593095273add54
James Cox <j-cox03@hotmail.com>
2017-12-21 13:10:28 +0000

    Schema: added source table to schema and updated some column lengths

--- database/schema.js
+++ database/schema.js
@@ -2,9 +2,9 @@ export const notesSchema = {
   name: "notes",
   columns: [
     {name: "id", type: "SERIAL8"},
-    {name: "title",  type: "VARCHAR(127)"},
+    {name: "title",  type: "VARCHAR(255)"},
     {name: "summary",  type: "TEXT"},
-    {name: "file",  type: "VARCHAR(127)"},
+    {name: "file",  type: "VARCHAR(255)"},
     {name: "creation_date",  type: "DATE"}
   ]
 }
@@ -13,7 +13,26 @@ export const tagsSchema = {
   name: "tags",
   columns: [
     {name: "id", type: "SERIAL8"},
-    {name: "name",  type: "VARCHAR(127)"}
+    {name: "name",  type: "VARCHAR(255)"}
+  ]
+}
+
+export const sourcesSchema ={
+  name: "sources",
+  columns: [
+    {name: "id", type: "SERIAL8"},
+    {name: "name", type: "VARCHAR(255)"},
+    {name: "type", type: "VARCHAR(255)"},
+    {name: "location", type: "TEXT"}
+  ]
+}
+
+export const noteSourcesSchema ={
+  name: "note_sources",
+  columns: [
+    {name: "id", type: "SERIAL8"},
+    {name: "note_id", type: "SERIAL8"},
+    {name: "source_id", type: "SERIAL8"}
   ]
 }
 
