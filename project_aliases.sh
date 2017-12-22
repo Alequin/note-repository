32204f489654fb653522703a372899731c58fa99
James Cox <j-cox03@hotmail.com>
2017-12-22 09:24:29 +0000

    Added new commands to seed database

--- project_aliases.sh
+++ project_aliases.sh
@@ -3,9 +3,11 @@ alias tst="npm test"
 alias bwp="npm run build-webpack"
 alias bjs="npm run build-es6"
 alias bdb="npm run build-database"
-alias bld=" npm run build-es6; npm run build-webpack"
+alias seed="npm run seed"
+alias reseed="npm run build-database; npm run seed"
+alias bld="npm run build-es6; npm run build-webpack"
 alias to-git="open https://github.com/Alequin/note-repository"
 
-alias clear-project-aliases="unalias strt tst bwp bjs bld to-git"
+alias clear-project-aliases="unalias strt tst bwp bjs bdb seed reseed bld to-git"
 
 echo "Project aliases loaded"
