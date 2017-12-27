e39516d47ea4c51fbb4ac317fadbc52bad5c29bf
James Cox <j-cox03@hotmail.com>
2017-12-27 14:54:07 +0000

    Made route to get a note by id

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -1,11 +1,17 @@
 import express from 'express'
 import requestAuth from "./../services/requestAuth.js"
 import {requestSummaryNotesWithTags} from "./../models/repos/summaryNotesRepo.js"
+import {requestFullNoteById} from "./../models/repos/noteRepo.js"
 
 const router = new express.Router();
 
 router.get('/', requestAuth, function(req, res, next){
   requestSummaryNotesWithTags().then((notes) => {res.json(notes)})
+})
+
+router.get('/:id', requestAuth, function(req, res, next){
+  requestFullNoteById(req.params.id)
+    .then((note) => {res.json(note)})
 })
 
 export default router
