f0d2e8e8b59b8690a6b1cdcb8a89e4310e32f1a8
James Cox <j-cox03@hotmail.com>
2017-12-27 14:52:40 +0000

    made functions to get a notes sources

---
+++ models/repos/sourceRepo.js
@@ -0,0 +1,33 @@
+import accessDatabase from "./../../database/accessDatabase.js"
+import {
+  sourcesSchema,
+  noteSourcesSchema,
+} from "./../../database/schema.js"
+
+export const findSourcesOfNote = function(note){
+  const sources = sourcesSchema.name
+  const sourcesCols = sourcesSchema.columns
+
+  const noteSources = noteSourcesSchema.name
+  const noteSourcesCols = noteSourcesSchema.columns
+
+  return accessDatabase.connect(
+    `SELECT
+      ${sources}.${sourcesCols.name.name},
+      ${sources}.${sourcesCols.type.name},
+      ${sources}.${sourcesCols.location.name}
+    FROM ${sources} INNER JOIN ${noteSources}
+    ON ${sources}.${sourcesCols.id.name} = ${noteSources}.${noteSourcesCols.sourceId.name}
+    WHERE ${noteSources}.${noteSourcesCols.noteId.name} = ${note.id}`
+  ).then((sources) => {
+    return sources.rows
+  })
+}
+
+export const attachSourcesToNote = function(note){
+  return findSourcesOfNote(note)
+    .then((sources) => {
+      note.setSources(sources)
+      return note
+    })
+}
